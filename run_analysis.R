6b41522eab48e48ffabcba5a060d45a88bc6f16c
attarwala <murtuza_attarwala@yahoo.com>
2014-04-20 13:28:11 -0700

    Add some more comments.

--- run_analysis.R
+++ run_analysis.R
@@ -35,7 +35,6 @@ run_analysis <- function(wd) {
     trainDataset <- read.table("train/X_train.txt", sep="", header=FALSE)
     
     # 1. Merge the training and test datasets to create one dataset
-    # We will merge the dataset by using the subject_id as the key
     # To do that first let's give meaningful column names to our dataset
     # The columns are nothing but features
     names(testDataset) <- features$V2
@@ -44,12 +43,22 @@ run_analysis <- function(wd) {
     # Add activity column so that dataset becomes more descriptive
     testDataset <- cbind(activity = testActivityLabels, testDataset)
     trainDataset <- cbind(activity = trainActivityLabels, trainDataset)
+
     # Now add subject_id column to our dataset
     testDataset <- cbind(subject_id = as.vector(testSubjects$V1), testDataset)
     trainDataset <- cbind(subject_id = as.vector(trainSubjects$V1), trainDataset)
+
+    # Merge the datasets using the default setting of using all the common columns
     mergedDataset <<- merge(testDataset[,unique(colnames(testDataset))], trainDataset[,unique(colnames(trainDataset))],all=TRUE)
+
+    # Extract the columns that have only mean and std deviation observations
+    # Keep subject_id and activity columns so that the extracted dataset is descriptive
     mean_std_dataset <<- mergedDataset[,grepl("subject_id|activity|mean\\(\\)|std\\(\\)",colnames(mergedDataset), ignore.case=FALSE)]
+
+    # Create molten dataset as a first step for creating the tidy dataset
     moltenDataset <- melt(mergedDataset,id=c("subject_id","activity"),measure.vars=colnames(mergedDataset)[-(1:2)])
+
+    # Create tidy dataset from the molten dataset
     finalDataset <<- dcast(moltenDataset, subject_id + activity ~ variable, mean)
     finalDataset
 }
